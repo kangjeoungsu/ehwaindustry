7be2972f9791a413d1bd110cab4c226bfc351a92
kangjeongsu1231 <jsk210712@gmail.com>
2024-08-29 18:11:31 +0900

    sec01 완료

--- script/js/swiper.js
+++ script/js/swiper.js
@@ -1,23 +1,24 @@
-import Swiper from 'swiper';
-import 'swiper/swiper-bundle.min.css';
 
-const swiper = new Swiper('.swiper-container', {
-    slidesPerView: 1,
-    spaceBetween: 10,
-    loop: true,
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-        renderBullet: function (index, className) {
-            return '<span class="' + className + '">' + ('0' + (index + 1)).slice(-2) + '</span>';
-            }
-    },
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-    autoplay: {
-        delay: 5000,
-        disableOnInteraction: false,
+
+document.addEventListener('DOMContentLoaded', function () {
+    const swiper = new Swiper('.swiper-container', {
+        slidesPerView: 1,
+        loop: true,
+        speed: 0,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+            renderBullet: function (index, className) {
+                return '<span class="' + className + '">' + ('0' + (index + 1)).slice(-2) + '</span>';
+                }
         },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        autoplay: {
+            delay: 5000,
+            disableOnInteraction: false,
+            },
+    });
 });
