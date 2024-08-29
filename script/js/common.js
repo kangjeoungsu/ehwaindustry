a79b0d46db543e389fec9a7f9629909bcff699b8
kangjeongsu1231 <jsk210712@gmail.com>
2024-08-29 13:19:21 +0900

    초기설정

---
+++ script/js/common.js
@@ -0,0 +1,10 @@
+window.addEventListener('scroll', function() {
+    const header = document.querySelector('header');
+    const scrollPosition = window.scrollY || document.documentElement.scrollTop;
+
+    if (scrollPosition > 50) { // 스크롤 위치에 따라 클래스 추가
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
